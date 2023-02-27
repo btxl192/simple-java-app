7774416a04676ac72b6f7354f2cb225b3bc2fe0a
Brandon Tan <btan192@gmail.com>
2023-02-27 08:38:22 +0000

    change2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am simple Java app234");
+        System.out.println("Hello there ! I am simple Java app2345");
     }
 
     public String getMessage() {
